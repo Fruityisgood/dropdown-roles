ef25f0e669d1dbde7d59fa65079aaec704660ae5
Fruity <penguinttskm@gmail.com>
2021-11-17 19:05:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-/*
-Copyright 2021 Whirl
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-
-
-*/
-
-
-
 // require the needed discord.js classes
 const { Client, Intents, MessageActionRow, MessageSelectMenu } = require('discord.js');
 
@@ -25,7 +10,7 @@ const {token , r1 , r2 ,r3 ,r4,r5 , r1m , r2m , r3m , r4m , r5m} = require('./co
 
 
 client.once('ready', () => {
-    console.log('Ready!');
+    console.log('Bot is online!');
 });
 
 client.on('interactionCreate', async interaction => {
